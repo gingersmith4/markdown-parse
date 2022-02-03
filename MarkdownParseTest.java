40305c02f8e188db00cf566c8f2bedc4a49adbaf
gingersmith4 <g4smith@ucsd.edu>
2022-02-02 17:52:21 -0800

    Fixed addition test

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -14,7 +14,7 @@ java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnit
 public class MarkdownParseTest {
     @Test
     public void addition() {
-        assertEquals(2, 1 + 2);
+        assertEquals(2, 1 + 1);
     }
 
     @Test
