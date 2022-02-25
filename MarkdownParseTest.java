8324de35da8ad07efe4db5ab26c9a7aa6f839ab3
gingersmith4 <g4smith@ucsd.edu>
2022-02-24 21:32:51 -0800

    Restored version with most updates and added snippet1 test

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -9,9 +9,11 @@ import java.io.IOException;
 
 /*To compile/run:
 javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar MarkdownParseTest.java 
+java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MarkdownParseTest
+
+for something else
 javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar MarkdownParseTest.java test-file.md 
 
-java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MarkdownParseTest
 */
 
 public class MarkdownParseTest {
@@ -66,21 +68,25 @@ public class MarkdownParseTest {
         assertEquals(linkTester, links);
     }
 
-    /**@Test
-    public void testGetLinksQuiz() throws IOException{
-        ArrayList<String> linkTester = new ArrayList<>();
-        linkTester.add("a-link.html");
-
-        Path fileName = Path.of("test-file-quiz.md");
-	    String contents = Files.readString(fileName);
-        ArrayList<String> links = MarkdownParse.getLinks(contents);
-        assertEquals(linkTester, links);
-    }*/
-
-    @Test
+    /*@Test
     public void testSpaceAfterParen() {
         String contents = "[title]( space-in-url.com)";
         List<String> expect = List.of("space-in-url.com");
         assertEquals(expect, MarkdownParse.getLinks(contents));
+    }*/
+
+    @Test
+    public void snippet1() throws IOException{
+        ArrayList<String> linkTester = new ArrayList<>();
+        //based on VSCode preview expect no correct links
+        linkTester.add("url.com");
+        linkTester.add("`google.com");
+        linkTester.add("google.com");
+        linkTester.add("ucsd.edu");
+
+        Path fileName = Path.of("snippet1.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        assertEquals(linkTester, links);
     }
 }
